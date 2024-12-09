e0ac1842306c8f81bfe445d06c866e19aa5dd97b
Kseniia Tretiakova <ktretyakova1927@gmail.com>
2024-12-09 13:32:02 +0300

    code

---
+++ src/components/ContactForm/ContactForm.jsx
@@ -0,0 +1,53 @@
+import React from "react";
+import { Formik, Form, Field, ErrorMessage } from "formik";
+import * as Yup from "yup";
+import s from "./ContactForm.module.css";
+import { useDispatch } from "react-redux";
+import { addContact } from "../../redux/contactsOps";
+
+const validationSchema = Yup.object({
+  name: Yup.string()
+    .required("Name is required")
+    .min(3, "Name must be at least 3 characters")
+    .max(50, "Name must be at most 50 characters"),
+  number: Yup.string()
+    .required("Phone number is required")
+    .min(3, "Phone must be at least 3 characters")
+    .max(50, "Phone must be at most 50 characters"),
+});
+
+const ContactForm = () => {
+  const dispatch = useDispatch();
+
+  const handleSubmit = (values, { resetForm }) => {
+    dispatch(addContact(values));
+    resetForm();
+  };
+  return (
+    <Formik
+      initialValues={{ name: "", number: "" }}
+      validationSchema={validationSchema}
+      onSubmit={handleSubmit}
+    >
+      {() => (
+        <Form className={s.form}>
+          <label className={s.label}>
+            Name
+            <Field type="text" name="name" className={s.field} />
+            <ErrorMessage name="name" component="div" className={s.error} />
+          </label>
+          <label className={s.label}>
+            Phone Number
+            <Field type="text" name="number" className={s.field} />
+            <ErrorMessage name="number" component="div" className={s.error} />
+          </label>
+          <button type="submit" className={s.button}>
+            Add Contact
+          </button>
+        </Form>
+      )}
+    </Formik>
+  );
+};
+
+export default ContactForm;
